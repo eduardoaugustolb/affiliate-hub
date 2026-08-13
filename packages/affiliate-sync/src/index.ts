0f03fce05f55f504794011db155239792179fa1b
Eduardo Augusto Lima Bueno <eduardoaugustolimabueno@outlook.com.br>
2026-08-13 18:37:33 -0300

    Remove index.ts from tracking

--- packages/affiliate-sync/src/index.ts
+++
@@ -1,3 +0,0 @@
-export type { AffiliateProduct, AffiliateProvider } from './application/ports/AffiliateProvider'
-export type { TaskScheduler } from './application/ports/TaskScheduler'
-export { AffiliateLink } from './domain/AffiliateLink'
