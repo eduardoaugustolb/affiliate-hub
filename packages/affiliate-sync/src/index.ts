984a90f21ba8cdbaf4345276f75d001bc8eb3226
Eduardo Augusto Lima Bueno <eduardoaugustolimabueno@outlook.com.br>
2026-08-13 18:35:14 -0300

    feat(affiliate-sync): adiciona estrutura inicial do módulo

---
+++ packages/affiliate-sync/src/index.ts
@@ -0,0 +1,3 @@
+export type { AffiliateProduct, AffiliateProvider } from './application/ports/AffiliateProvider'
+export type { TaskScheduler } from './application/ports/TaskScheduler'
+export { AffiliateLink } from './domain/AffiliateLink'
